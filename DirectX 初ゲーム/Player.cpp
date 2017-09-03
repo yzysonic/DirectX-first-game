f87092807d3daee3ef302c712c83a01da78e06aa
YangTzui <yzysonic@gmail.com>
2017-09-03 16:53:38 +0900

    プレイヤーコントロールの仕様変更。

--- DirectX 初ゲーム/Player.cpp
+++ DirectX 初ゲーム/Player.cpp
@@ -8,33 +8,49 @@
 void initPlayer(Object *thiz)
 {
 	thiz->polygon = newPolygon(thiz, LAYER_PLAYER, TEX_PLAYER);
-	thiz->transform->position = Vector3(600, 600, 0.0f);
+	thiz->transform->position = Vector3(0.0f, 0.0f, 0.0f);
 	
 }
 
 void updatePlayer(Object *thiz)
 {
-	if (GetKeyboardPress(DIK_DOWN))
+	float speed = 5.0f;
+	float boost = 1.0f;
+	
+	if (GetKeyboardPress(DIK_LSHIFT))
+		boost = 2.0f;
+
+	if (GetKeyboardPress(DIK_LALT))
 	{
-		thiz->transform->position.y += 5.0f;
+		if (GetKeyboardPress(DIK_DOWN))
+		{
+			thiz->transform->position.z -= 0.02f*boost;
+		}
+		else if (GetKeyboardPress(DIK_UP))
+		{
+			thiz->transform->position.z += 0.02f*boost;
+		}
 	}
-	else if (GetKeyboardPress(DIK_UP))
+	else
 	{
-		thiz->transform->position.y -= 5.0f;
+		if (GetKeyboardPress(DIK_DOWN))
+		{
+			thiz->transform->position.y += speed*boost;
+		}
+		else if (GetKeyboardPress(DIK_UP))
+		{
+			thiz->transform->position.y -= speed*boost;
+		}
 	}
 
 	if (GetKeyboardPress(DIK_RIGHT))
 	{
-		thiz->transform->position.x += 5.0f;
+		thiz->transform->position.x += speed*boost;
 	}
 	else if (GetKeyboardPress(DIK_LEFT))
 	{
-		thiz->transform->position.x -= 5.0f;
+		thiz->transform->position.x -= speed*boost;
 	}
-
-	//thiz->transform->position.x += 100*GetDeltaTime();
-	if (thiz->transform->position.x > SCREEN_WIDTH)
-		thiz->transform->position.x = 0;
 
 }
 
