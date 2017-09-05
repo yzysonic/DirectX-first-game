eb35f14b6b1e858b9b084c85cf6197c6df23db69
YangTzui <yzysonic@gmail.com>
2017-09-06 03:53:22 +0900

    カラー補間関数の追加。

--- DirectX 初ゲーム/Lerp.cpp
+++ DirectX 初ゲーム/Lerp.cpp
@@ -20,14 +20,19 @@ Vector3 LerpV3(Vector3 a, Vector3 b, float t)
 		return (a + (b - a)*t);
 }
 
-//Color LerpC(Color a, Color b, float t)
-//{
-//	if (t >= 1)
-//		return b;
-//	else if (t <= 0)
-//		return a;
-//	else
-//	{
-//		return (a + (b - a)*t);
-//	}
-//}
+Color LerpC(Color a, Color b, float t)
+{
+	if (t >= 1)
+		return b;
+	else if (t <= 0)
+		return a;
+	else
+	{
+		Color result = 0;
+		for (int i = 0; i < 4; i++)
+		{
+			result += (int)Lerpf((a & (0x000000ff << (8 * i))) >> (8 * i), (b & (0x000000ff << (8 * i))) >> (8 * i), t) << (8 * i);
+		}
+		return result;
+	}
+}
--- DirectX 初ゲーム/Lerp.h
+++ DirectX 初ゲーム/Lerp.h
@@ -3,4 +3,4 @@
 
 float Lerpf(float a, float b, float t);
 Vector3 LerpV3(Vector3 a, Vector3 b, float t);
-//Color LerpC(Color a, Color b, float t);
+Color LerpC(Color a, Color b, float t);
